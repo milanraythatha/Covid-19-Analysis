f6bf7cba14a17cf739e4722140aa88a4a4809152
Milan Raythatha <163181390+milanraythatha@users.noreply.github.com>
2024-04-06 16:50:07 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-# Covid-19-Analysis
+# Covid-SQL-Analytics
+<div align="left">
+   <a href="https://opensource.org/osd">
+      <img src="https://img.shields.io/badge/Open%20Source-%2328a745" alt="open-source"/>
+   </a>
+   <a href="https://opensource.org/license/mit/">
+      <img src="https://img.shields.io/badge/License-MIT-green" alt="mit-license"/>
+   </a>
+  <a href="https://www.postgresql.org/">
+    <img src="https://img.shields.io/badge/PostgreSQL-%23007ACC?logo=postgresql&logoColor=white" alt="PostgreSQL"/>
+  </a>
+   </div>
+
+## About:
+This repository serves as a resource for SQL-based data analysis using PostgreSQL, focusing on Covid-19 data. It includes the <a href="https://github.com/g0v1ndN/Covid-SQL-Analytics/blob/main/analyze_covid.sql">code</a>, <a href="https://github.com/g0v1ndN/Covid-SQL-Analytics/tree/main/covid_data">curated data</a>, and <a href="https://github.com/g0v1ndN/Covid-SQL-Analytics#observations--findings">observations</a> derived from detailed query analysis, making it a valuable reference for those interested in exploring this dataset.
+
+## Usage Instructions:
+
+### Prerequisites:
+- SQL and PostgreSQL Knowledge: A fundamental understanding of SQL concepts and experience with PostgreSQL will be beneficial as you navigate through the queries and analysis presented here.
+- Development Environment: Set up a development environment with PostgreSQL installed. You can download PostgreSQL from its <a href="https://www.postgresql.org/">official website</a> and follow the installation instructions provided.
+- Basic Data Analysis Skills: Familiarity with data analysis concepts will help you better understand the insights derived from the queries and observations shared in this repository.
+
+### Downloading or Cloning the Repository:
+1. Click on the "Code" button on the top right corner of this repository page.
+2. Click on "Download ZIP" to download a compressed zip file of the repository.
+3. Extract the contents of the zip file to a folder on your computer.
+
+<p align="center"><b> OR </b></p>
+
+1. Open your terminal or command prompt and navigate to the directory where you want to clone the repository.
+2. Run the command `git clone https://github.com/g0v1ndN/Covid-SQL-Analytics` to clone the repository to your local machine.
+> [!NOTE]
+> You need to have Git installed on your computer. If you don't have it installed, please download and install Git from the [official website](https://git-scm.com).
+
+### Exploring the Code: 
+If you want to dive deep into the what this repository brings to the table, familiarize yourself with the main analysis code located in the analyze_covid.sql file. This code is split into three key parts:
+
+1. Initial Setup Queries: These queries establish the foundation by creating the necessary database, tables, and importing the Covid-19 data.
+2. Data Analysis Queries for India: This section contains SQL queries tailored to analyzing Covid-19 data specific to India. 
+3. Global COVID-19 Data Analysis Queries: This segment comprises SQL queries aimed at dissecting and analyzing the global COVID-19 data landscape. It provides insight to cross-country comparisons and trends in the pandemic's data.
+
+## Observations & Findings:
+> [!IMPORTANT]
+> The data used in this project covers the period from January 1, 2020 to August 16, 2023. Please note that the source website's data might have been updated since the start of this project.
+
+### Understanding the COVID-19 Situation in India
+- COVID-19 was first reported in India on <b>January 30, 2020</b>.
+- As of the latest data, the infection percentage in India is <b>3.18%</b>, indicating a relatively <b>moderate spread</b> of COVID-19 within the population.
+- The peak of new COVID-19 cases was recorded on <b>May 2021</b>, with a notable count of <b>9,284,558</b> new cases.
+- Currently, the mortality rate in India stands at <b>1.18%</b>, indicating a comparatively low fatality rate among confirmed cases.
+- <b>72.50%</b> of the population in India has received at least one dose of the Covid-19 vaccine, and <b>67.17%</b> are fully vaccinated.
+- India has reported a significant number of COVID-19 deaths, totaling <b>531,922</b>, which corresponds to <b>0.04%</b> of its population.
+
+### Global COVID-19 Landscape
+
+#### COVID-19 Outbreak and Global Data:
+- The COVID-19 outbreak, which originated in <b>China</b>, was officially reported on <b>January 4, 2020</b>.
+- The latest data reveals a COVID-19 infection rate of approximately <b>9.65%</b>.
+- It also discloses a mortality rate of <b>0.90%</b>, reflecting a relatively low fatality rate among confirmed cases.
+
+#### Infection Statistics:  
+- As of the current data, the <b>top 5 hotspots</b> with the highest infection percentages are Cyprus (73.76%), San Marino (72.37%), Brunei (69.09%), Austria (68.03%), and South Korea (65.42%).
+- In contrast, North Korea and Turkmenistan have <b>zero cases</b>, while Yemen, Niger, and Chad share the lowest infection rate of <b>0.04%</b>.
+- The highest number of new COVID-19 cases globally occurred on <b>January 2022</b>, with a total of <b>91,806,015</b> new cases reported.
+
+#### Vaccination Statistics:
+- Notably, vaccinations have been administered to more than <b>5.6 billion people</b>, accounting for <b>70.48%</b> of the population.
+- Gibraltar, Tokelau, United Arab Emirates, Qatar, Nauru, Brunei, and Pitcairn have successfully attained vaccination percentages exceeding <b>99%</b>.
+- Vaccination rates vary across <b>income categories</b>, with 32.54% of low-income, 79.86% of high-income, 66.28% of lower-middle-income, and 83.42% of upper-middle-income individuals having been vaccinated at least once.
+
+#### Regional Deaths and COVID-19 Toll:
+- Peru, Bulgaria, Bosnia and Herzegovina, Hungary, and North Macedonia exhibit higher death percentages, with rates ranging from <b>0.47% to 0.65%</b>.
+- North America leads with the highest death count (1,127,152), followed by South America (704,659), Asia (531,922), Europe (399,938), Africa (102,595) and Oceania (22,618) on a <b>continental basis</b>.
+- The <b>countries</b> with the highest COVID-19 death toll are as follows: United States (1,127,152 deaths), Brazil (704,659 deaths), India (531,922 deaths), Russia (399,938 deaths), and Mexico (334,336 deaths).
+
+## External Resources:
+
+### Data Inclusion
+- **Data Source:** Our World in Data
+- **Citation:** Edouard Mathieu, Hannah Ritchie, Lucas Rodés-Guirao, Cameron Appel, Charlie Giattino, Joe Hasell, Bobbie Macdonald, Saloni Dattani, Diana Beltekian, Esteban Ortiz-Ospina, and Max Roser (2020) - "Coronavirus Pandemic (COVID-19)". Published online at <a href="https://ourworldindata.org">OurWorldInData.org</a>.
+- **License:** CC-BY-4.0 License
+
+The Creative Commons Attribution (CC-BY) License is an open-source license that permits usage, modification, and distribution of the content, provided you attribute the original creator.
+
+## License: 
+This project is licensed under the terms & conditions (T&Cs) of the MIT license. If you're interested in reading more about it, please take a look at the <a href="https://github.com/g0v1ndN/Covid-SQL-Analytics/blob/main/LICENSE">LICENSE</a> document.
---
+++ analyze_covid.sql
@@ -0,0 +1,365 @@
+/* 
+SQL script for database setup, data analysis, and visualization of COVID-19 data.
+Showcases SQL skills in database management, data import, aggregation, table joins, calculation, subqueries, filtering, result ordering, and data analysis.
+*/
+
+
+-- Initial Setup Queries:
+
+-- Create a PL/pgSQL function to declare and assign variables.
+DO $$
+DECLARE
+    deaths_file_path TEXT;
+    vaccinations_file_path TEXT;
+BEGIN
+    deaths_file_path := '/path/to/CovidDeaths.csv';
+    vaccinations_file_path := '/path/to/CovidVaccinations.csv';
+END;
+$$;
+
+-- Create a database to store and analyze COVID-19 related data.
+CREATE DATABASE analyze_covid;
+
+-- Create a table to store COVID-19 deaths data.
+CREATE TABLE IF NOT EXISTS public.covid_deaths
+(
+    iso_code character varying(10),
+    continent character varying(50),
+    location character varying(100),
+    date date,
+    population bigint,
+    total_cases bigint,
+    new_cases bigint,
+    new_cases_smoothed numeric,
+    total_deaths bigint,
+    new_deaths bigint,
+    new_deaths_smoothed numeric,
+    total_cases_per_million numeric,
+    new_cases_per_million numeric,
+    new_cases_smoothed_per_million numeric,
+    total_deaths_per_million numeric,
+    new_deaths_per_million numeric,
+    new_deaths_smoothed_per_million numeric
+);
+
+-- Import COVID-19 deaths data from a CSV file into the "covid_deaths" table.
+COPY public.covid_deaths FROM deaths_file_path DELIMITER ',' CSV HEADER;
+
+-- Create a table to store COVID-19 vaccination data.
+CREATE TABLE IF NOT EXISTS public.covid_vaccinations (
+    iso_code VARCHAR(10),
+    continent VARCHAR(50),
+    location VARCHAR(100),
+    date DATE,
+    total_vaccinations BIGINT,
+    people_vaccinated BIGINT,
+    people_fully_vaccinated BIGINT,
+    new_vaccinations BIGINT,
+    new_vaccinations_smoothed BIGINT
+);
+
+-- Import COVID-19 vaccination data from a CSV file into the "covid_vaccinations" table.
+COPY public.covid_vaccinations FROM vaccinations_file_path DELIMITER ',' CSV HEADER;
+
+-- Retrieve and compare the starting and ending dates of COVID-19 data from both tables to ensure data consistency.
+SELECT
+    (SELECT MIN(date) FROM public.covid_deaths) AS covid_deaths_start_date,
+    (SELECT MAX(date) FROM public.covid_deaths) AS covid_deaths_end_date,
+    (SELECT MIN(date) FROM public.covid_vaccinations) AS covid_vaccinations_start_date,
+    (SELECT MAX(date) FROM public.covid_vaccinations) AS covid_vaccinations_end_date;
+
+-- Create an index on the 'location' column of the 'public.covid_deaths' table.
+CREATE INDEX IF NOT EXISTS location_idx ON public.covid_deaths (location);
+
+-- End of Initial Setup Queries.
+
+
+-- Data Analysis Queries for India:
+
+-- Calculate the date when Covid was first reported in India.
+SELECT
+    location,
+    MIN(date) AS first_reported_date
+FROM
+    public.covid_deaths
+WHERE
+    location = 'India'
+    AND total_cases > 0
+GROUP BY
+    location;
+
+-- Calculate and format the percentage of population infected with COVID-19 in India.
+SELECT
+    location,
+    date,
+    population,
+    total_cases,
+    ROUND((total_cases::numeric / population) * 100, 2) AS percent_population_infected
+FROM
+    public.covid_deaths
+WHERE
+    location = 'India'
+    AND date = '2023-08-16' -- This date was chosen because it reflects the dataset's last update.
+ORDER BY
+    location, date;
+
+-- Calculates monthly new cases in India, sorted by highest counts.
+SELECT
+    DATE_TRUNC('month', date) AS month,
+    SUM(new_cases) AS total_new_cases
+FROM
+    public.covid_deaths
+WHERE
+    location = 'India'
+GROUP BY
+    month
+ORDER BY
+    total_new_cases DESC;
+
+-- Calculates and format the mortality rate for Covid-related data in India.
+SELECT
+    location,
+    date,
+    total_cases,
+    total_deaths,
+    ROUND((total_deaths::numeric / total_cases) * 100, 2) AS death_percentage
+FROM
+    public.covid_deaths
+WHERE
+    location = 'India'
+    AND date = '2023-08-16' -- This date was chosen because it reflects the dataset's last update.
+ORDER BY
+    location, date;
+
+-- Calculate the vaccination percentage for India.
+SELECT
+    v.location AS country,                  
+    v.date,                                 
+    d.population,                           
+    v.people_vaccinated,                    
+    ROUND((v.people_vaccinated::numeric / d.population) * 100, 2) AS vaccination_percentage                                           
+FROM
+    public.covid_vaccinations v             
+JOIN
+    public.covid_deaths d                   
+    ON v.location = d.location              
+    AND v.date = d.date
+WHERE
+    v.location = 'India'              
+    AND v.date = '2023-08-16' -- This date was chosen because it reflects the dataset's last update.               
+ORDER BY
+    country, date;                                   
+
+-- Retrieve the complete vaccination percentage for India.
+SELECT
+    v.location AS country,
+    v.date,
+    d.population,
+    v.people_fully_vaccinated,
+    ROUND((v.people_fully_vaccinated::numeric / d.population) * 100, 2) AS fully_vaccination_percentage
+FROM
+    public.covid_vaccinations v
+JOIN
+    public.covid_deaths d ON v.location = d.location AND v.date = d.date
+WHERE
+    v.location = 'India'
+    AND v.date = '2023-08-16' -- This date was chosen because it reflects the dataset's last update.
+ORDER BY
+    location, date;
+
+-- Calculate total deaths and death percentage for India.
+SELECT
+    SUM(new_deaths) AS total_deaths,
+    ROUND(SUM(new_deaths)::numeric / (SELECT population FROM public.covid_deaths WHERE location = 'India' LIMIT 1) * 100, 2) AS death_percentage
+FROM
+    public.covid_deaths
+WHERE
+    location = 'India'
+    AND new_cases > 0;
+
+-- End of Data Analysis Queries for India.
+
+
+-- Global COVID-19 Data Analysis Queries:
+
+-- Find the location and date of the first reported COVID-19 case globally.
+SELECT
+    location,
+    MIN(date) AS first_reported_date
+FROM
+    public.covid_deaths
+WHERE
+    continent IS NOT NULL -- Filter out invalid rows like "World".
+    AND total_cases > 0
+GROUP BY
+    location
+ORDER BY
+    first_reported_date
+LIMIT 1;
+
+-- Calculate the infection rate of COVID-19 as of today.
+SELECT
+    SUM(new_cases) AS total_cases,
+    population,
+    ROUND(SUM(new_cases::numeric) / population * 100, 2) AS infection_rate
+FROM
+    public.covid_deaths
+WHERE
+    location = 'World'
+    AND date <= '2023-08-16' -- This date was chosen because it reflects the dataset's last update.
+GROUP BY
+    population;
+
+-- Calculate the mortality rate of COVID-19 as of today.
+SELECT
+    SUM(new_cases) AS total_cases,
+    SUM(new_deaths) AS total_deaths,
+    ROUND(SUM(new_deaths::numeric) / SUM(new_cases) * 100, 2) AS mortality_rate
+FROM
+    public.covid_deaths
+WHERE
+    continent IS NOT NULL -- Filter out invalid rows like "World".
+    AND date <= '2023-08-16' -- This date was chosen because it reflects the dataset's last update.
+ORDER BY
+    total_cases, total_deaths;
+
+-- Calculate the percentage of the population that is globally infected by COVID-19.
+SELECT
+    location,
+    population,
+    ROUND((MAX(total_cases)::numeric / population) * 100, 2) AS percent_population_infected 
+FROM
+    public.covid_deaths
+GROUP BY
+    location, population
+ORDER BY
+    percent_population_infected DESC NULLS LAST;
+
+-- Calculates monthly new cases globally, sorted by highest counts.
+SELECT
+    DATE_TRUNC('month', date) AS month,
+    SUM(new_cases) AS total_new_cases
+FROM
+    public.covid_deaths
+WHERE
+    location = 'World'
+GROUP BY
+    month
+ORDER BY
+    total_new_cases DESC;
+
+-- Retrieve vaccination data and calculate global vaccination percentage.
+SELECT
+    v.location AS country,                  
+    v.date,                                 
+    d.population,                           
+    v.people_vaccinated,                    
+    ROUND((v.people_vaccinated::numeric / d.population) * 100, 2) AS vaccination_percentage                                           
+FROM
+    public.covid_vaccinations v             
+JOIN
+    public.covid_deaths d                   
+    ON v.location = d.location              
+    AND v.date = d.date
+WHERE
+    v.location = 'World'              
+    AND v.date = '2023-08-16' -- This date was chosen because it reflects the dataset's last update.               
+ORDER BY
+    country, date; 
+
+-- Retrieves the latest COVID-19 vaccination percentages for countries based on population.
+SELECT
+    v.location AS country,
+    MAX(v.date) AS date,
+    d.population,
+    MAX(v.people_vaccinated) AS people_vaccinated,
+    ROUND((MAX(v.people_vaccinated)::numeric / d.population) * 100, 2) AS vaccination_percentage
+FROM
+    public.covid_vaccinations v
+JOIN
+    public.covid_deaths d ON v.location = d.location AND v.date = d.date
+WHERE
+    v.continent IS NOT NULL -- Filter out invalid rows like "World".
+    AND v.date <= '2023-08-16' -- This date was chosen because it reflects the dataset's last update.
+    AND v.people_vaccinated IS NOT NULL
+GROUP BY
+    v.location, d.population
+ORDER BY
+    vaccination_percentage DESC;
+
+-- Retrieve data of people vaccinated from different income strata.
+SELECT DISTINCT ON (v.location)
+    v.location AS category,
+    MAX(v.date) AS max_vaccination_date,
+    d.population,
+    v.people_vaccinated,
+    ROUND(v.people_vaccinated::numeric / d.population * 100, 2) AS vaccination_percentage
+FROM
+    public.covid_vaccinations v
+JOIN
+    public.covid_deaths d ON v.location = d.location AND v.date = d.date
+WHERE
+    v.location ILIKE '%income%'
+GROUP BY
+    v.location, d.population, v.people_vaccinated
+ORDER BY
+    v.location, max_vaccination_date DESC;
+
+-- This query retrieves data for the "Low income" strata with its maximum date used.
+-- Addresses missing data for the "Low income" category in the last query.
+SELECT
+    v.location AS category,
+    MAX(v.date) AS max_date_used,
+    d.population,
+    v.people_vaccinated,
+    ROUND((v.people_vaccinated::numeric / d.population) * 100, 2) AS vaccination_percentage
+FROM
+    public.covid_vaccinations v
+JOIN
+    public.covid_deaths d ON v.location = d.location AND v.date = d.date
+WHERE
+    v.location = 'Low income'
+GROUP BY
+    v.location, d.population, v.people_vaccinated
+ORDER BY
+    max_date_used DESC;
+
+-- Calculate the death percentage of the population based on COVID-19 data.
+SELECT
+    location,
+    population,
+    ROUND((MAX(total_deaths)::numeric / population) * 100, 2) AS death_percentage 
+FROM
+    public.covid_deaths
+GROUP BY
+    location, population
+ORDER BY
+    death_percentage DESC NULLS LAST;
+
+-- Retrieve the maximum recorded COVID-19 death count for each continent.
+SELECT
+    continent,
+    MAX(total_deaths::numeric) AS total_death_count
+FROM
+    public.covid_deaths
+WHERE
+    continent IS NOT NULL -- Filter out invalid rows like "World".
+GROUP BY
+    continent
+ORDER BY
+    total_death_count DESC;
+
+-- Examine countries based on their highest recorded COVID-19 death counts.
+SELECT
+    location,
+    COALESCE(MAX(total_deaths), 0) AS max_total_deaths
+FROM
+    public.covid_deaths
+WHERE
+    continent IS NOT NULL -- Filter out invalid rows like "World".
+GROUP BY
+    location
+ORDER BY
+    max_total_deaths DESC;
+
+-- End of Global COVID-19 Data Analysis Queries.
